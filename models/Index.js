acd02cc86846dcef386edf250460ea308c547de0
Chris Cline <115193125+ccline29@users.noreply.github.com>
2023-03-06 20:17:24 -0600

    models

--- models/Index.js
+++ models/Index.js
@@ -1,39 +1,35 @@
-
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-Product.belongsTo(Category, {
-    foreignKey: 'category_id'
-});
-
-// Categories have many Products
-
+// Establishes "parent" one-to-many relationship between Category and Product
 Category.hasMany(Product, {
-    foreignKey: 'category_id'
-});
+  foreignKey: 'category_id', 
+  onDelete: 'CASCADE', 
+}); 
 
-// Products belongToMany Tags (through ProductTag)
+// Establishes "child" one-to-many relationship between Category and Product
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', 
+}); 
 
+// Establishes many-to-many relationship between Product and Tag through the pre-defined columns in ProductTag
 Product.belongsToMany(Tag, {
-    through: ProductTag,
-    foreignKey: 'product_id'
-});
+  through: ProductTag, 
+  foreignKey: 'product_id', 
+}); 
 
-// Tags belongToMany Products (through ProductTag)
-
+// Establishes many-to-many relationship between Product and Tag through the pre-defined columns in ProductTag
 Tag.belongsToMany(Product, {
-    through: ProductTag,
-    foreignKey: 'tag_id'
+  through: ProductTag,
+  foreignKey: 'tag_id', 
 });
 
 module.exports = {
-    Product,
-    Category,
-    Tag,
-    ProductTag,
+  Product,
+  Category,
+  Tag,
+  ProductTag,
 };
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,38 +5,36 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-    {
-        // define an id column
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        // define product_id column
-        product_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "product",
-                key: "id"
-            }
-        },
-        // define tag_id column
-        tag_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "tag",
-                key: "id"
-            }
-        }
-    },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'product_tag',
+  {
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    }, 
+    product_id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      references: {
+        model: 'product', 
+        key: 'id'
+      }
+    }, 
+    tag_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'tag', 
+        key: 'id'
+      }
     }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
 );
 
 module.exports = ProductTag;
